5261b4d1a436595c9c6252862d5d23499ffea5e0
Gabriel Trejo <gabrielTrejo433@gmail.com>
2025-08-30 23:30:13 -0500

    Add navLinks component for header navigation
    - Introduced a new navLinks component that serves as a placeholder for navigation links in the application header.
    - The component is structured using a section element with flexbox for alignment and spacing.

---
+++ src/shared/Header/nav-links.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ThemeSwitcher from './theme-switcher'
+
+const navLinks = () => {
+  return ( 
+    <section className='flex align-items-center gap-x-2'>
+        <ThemeSwitcher />
+        
+    </section>
+  )
+}
+
+export default navLinks
