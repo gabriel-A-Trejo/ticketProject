6ce4ed8d500708e83b81a13fd56e7ddc07bcca37
Gabriel Trejo <gabrielTrejo433@gmail.com>
2025-08-30 23:34:15 -0500

    Enhance header navigation with new navItems
    - Added navigation items for "All Tickets" and "My Tickets" in NavItems.tsx, utilizing icons from lucide-react for improved visual representation.
    - Introduced a closedClassName for styling transitions, enhancing the user experience in the header navigation.

--- src/shared/Header/constants/NavItems.tsx
+++ src/shared/Header/constants/NavItems.tsx
@@ -1,3 +1,21 @@
+import { NavItem } from "@/shared/types/types";
+import { HomePath, TicketsPath } from "./path";
+import { LucideLibrary, LucideBook} from "lucide-react";
+
 
 
 export const navItems: NavItem[] = [
+    {
+        title: 'All Tickets',
+        icon: <LucideLibrary />,
+        href: HomePath()
+    },
+    {
+        title: 'My Tickets',
+        icon: <LucideBook />,
+        href: TicketsPath()
+    }
+]
+
+export const closedClassName =
+  "text-background opacity-0 transition-all duration-300 group-hover:z-40 group-hover:ml-4 group-hover:rounded group-hover:bg-foreground group-hover:p-2 group-hover:opacity-100";
